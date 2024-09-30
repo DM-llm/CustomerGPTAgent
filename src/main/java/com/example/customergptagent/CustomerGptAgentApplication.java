8ab50bed00ed0da8c0c939d0e057a21b0c823416
hcy <1306574172@qq.com>
2024-09-30 15:16:37 +0800

    第一次

---
+++ src/main/java/com/example/customergptagent/CustomerGptAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.customergptagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerGptAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerGptAgentApplication.class, args);
+	}
+
+}
